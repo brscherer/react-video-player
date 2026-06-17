f318c32f380ec1e4251735665b5c13a3876fc74f
Bruno Scherer <bruno.raphael96@gmail.com>
2026-06-16 21:04:17 -0300

    Update video

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <main style={{ maxWidth: 800, margin: '40px auto', padding: '0 16px' }}>
       <h1>Video Player</h1>
-      <VideoPlayer src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" />
+      <VideoPlayer src="/video.mp4" />
     </main>
   )
 }
